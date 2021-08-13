1e02ab02f9e8efdd7ef30a78a35d639abe7b4272
Qiang Wang <wang-q@outlook.com>
2021-08-14 01:37:32 +0800

    Update cli.rs

--- tests/cli.rs
+++ tests/cli.rs
@@ -14,6 +14,22 @@ fn command_invalid() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn command_status_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("garr")?;
+    let output = cmd
+        .arg("status")
+        .arg("test")
+        .output()
+        .unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert!(stdout.lines().count() > 20);
+    assert!(stdout.contains("Running SET commands"));
+
+    Ok(())
+}
+
+#[test]
 fn command_env() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("garr")?;
     let output = cmd
@@ -44,22 +60,6 @@ fn command_env_env() -> Result<(), Box<dyn std::error::Error>> {
 
     assert_eq!(stdout.lines().count(), 6);
     assert!(stdout.contains("REDIS_PASSWORD='mYpa$$'"), "modified values");
-
-    Ok(())
-}
-
-#[test]
-fn command_status_test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("garr")?;
-    let output = cmd
-        .arg("status")
-        .arg("test")
-        .output()
-        .unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
-
-    assert!(stdout.lines().count() > 20);
-    assert!(stdout.contains("Running SET commands"));
 
     Ok(())
 }
