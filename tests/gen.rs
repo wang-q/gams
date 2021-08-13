c0a8f2c97b5cd53f49da3f2adcb3708ddcfe411f
Qiang Wang <wang-q@outlook.com>
2021-08-14 01:32:39 +0800

    Misc changes

--- tests/gen.rs
+++ tests/gen.rs
@@ -2,22 +2,6 @@ use assert_cmd::prelude::*; // Add methods on commands
 use std::process::Command; // Run programs
 
 #[test]
-fn command_status_drop() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("garr")?;
-    let output = cmd
-        .arg("status")
-        .arg("drop")
-        .output()
-        .unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
-
-    assert_eq!(stdout.lines().count(), 1);
-    assert!(stdout.contains("OK"));
-
-    Ok(())
-}
-
-#[test]
 fn command_gen() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("garr")?;
     let output = cmd
@@ -31,22 +15,6 @@ fn command_gen() -> Result<(), Box<dyn std::error::Error>> {
 
     assert_eq!(stdout.lines().count(), 14);
     assert!(stdout.contains("There are 3 contigs"));
-
-    Ok(())
-}
-
-#[test]
-fn command_status_dump() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("garr")?;
-    let output = cmd
-        .arg("status")
-        .arg("dump")
-        .output()
-        .unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
-
-    assert_eq!(stdout.lines().count(), 1);
-    assert!(stdout.contains("OK"));
 
     Ok(())
 }
