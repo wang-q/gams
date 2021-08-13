e470aec2d6bd99e9302f8b480923a6d6a2dbf236
Qiang Wang <wang-q@outlook.com>
2021-08-14 00:53:25 +0800

    Update cli.rs
    Tests for `garr status`

--- tests/cli.rs
+++ tests/cli.rs
@@ -47,3 +47,51 @@ fn command_env_env() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn command_status_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("garr")?;
+    let output = cmd
+        .arg("status")
+        .arg("test")
+        .output()
+        .unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert!(stdout.lines().count() > 20);
+    assert!(stdout.contains("Running SET commands"));
+
+    Ok(())
+}
+
+#[test]
+fn command_status_drop() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("garr")?;
+    let output = cmd
+        .arg("status")
+        .arg("drop")
+        .output()
+        .unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert_eq!(stdout.lines().count(), 1);
+    assert!(stdout.contains("OK"));
+
+    Ok(())
+}
+
+#[test]
+fn command_status_dump() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("garr")?;
+    let output = cmd
+        .arg("status")
+        .arg("dump")
+        .output()
+        .unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert_eq!(stdout.lines().count(), 1);
+    assert!(stdout.contains("OK"));
+
+    Ok(())
+}
