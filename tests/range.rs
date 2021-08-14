71b6dea7a4aa814356776a58bf4e58f1e3f22278
Qiang Wang <wang-q@outlook.com>
2021-08-15 05:53:20 +0800

    Update range.rs

--- tests/range.rs
+++ tests/range.rs
@@ -20,6 +20,8 @@ fn command_range() -> Result<(), Box<dyn std::error::Error>> {
     let output = cmd
         .arg("range")
         .arg("tests/S288c/spo11_hot.pos.txt")
+        .arg("--tag")
+        .arg("spo11")
         .output()
         .unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
