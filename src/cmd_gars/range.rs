2866b073b21fc38c2f72dccf347a8c11bd529616
Qiang Wang <wang-q@outlook.com>
2023-08-18 14:26:30 +0800

    Much faster

--- src/cmd_gars/range.rs
+++ src/cmd_gars/range.rs
@@ -1,7 +1,7 @@
 use clap::*;
-use gars::*;
 use intspan::*;
 use redis::Commands;
+use std::collections::BTreeMap;
 use std::io::BufRead;
 
 // Create clap subcommand arguments
@@ -21,12 +21,22 @@ ID - format!("range:{}:{}", ctg_id, serial)
                 .num_args(1..)
                 .help("Sets the input file to use"),
         )
+        .arg(
+            Arg::new("size")
+                .long("size")
+                .num_args(1)
+                .default_value("100")
+                .value_parser(value_parser!(usize))
+                .help("Batch size for one Redis submission"),
+        )
 }
 
 // command implementation
 pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
+    let opt_size = *args.get_one::<usize>("size").unwrap();
+
     // redis connection
-    let mut conn = connect();
+    let mut conn = gars::connect();
 
     // index of ctgs
     let lapper_of = gars::get_idx_ctg(&mut conn);
@@ -34,6 +44,10 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
     // processing each file
     for infile in args.get_many::<String>("infiles").unwrap() {
         let reader = reader(infile);
+
+        // ctg_id, Range
+        // act as a sorter
+        let mut ranges_of: BTreeMap<String, Vec<Range>> = BTreeMap::new();
 
         for line in reader.lines().map_while(Result::ok) {
             let mut rg = Range::from_str(&line);
@@ -47,24 +61,70 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
                 continue;
             }
 
-            // Redis counter
-            let serial: isize = conn.incr(format!("cnt:range:{}", ctg_id), 1).unwrap();
+            ranges_of
+                .entry(ctg_id)
+                .and_modify(|v| v.push(rg))
+                .or_insert(Vec::new());
+        }
+
+        // ctg_id, Range
+        let mut ctg_ranges: Vec<(String, Range)> = vec![];
+        for k in ranges_of.keys() {
+            for r in ranges_of.get(k).unwrap() {
+                ctg_ranges.push((k.to_string(), r.clone()));
+            }
+        }
+
+        // total number of ranges
+        eprintln!("There are {} ranges in this file", ctg_ranges.len());
+
+        // start serial of each ctg
+        // To minimize expensive Redis operations, locally increment the serial number
+        // For each ctg, we increase and set the counter only once
+        let mut serial_of: BTreeMap<String, i32> = BTreeMap::new();
+
+        let mut batch = &mut redis::pipe();
+
+        for (i, (ctg_id, rg)) in ctg_ranges.iter().enumerate() {
+            // prompts
+            if i > 1 && i % opt_size == 0 {
+                let _: () = batch.query(&mut conn).unwrap();
+                batch.clear();
+            }
+            if i > 1 && i % (opt_size * 10) == 0 {
+                eprintln!("Read {} records", i);
+            }
+
+            // serial and id
+            if !serial_of.contains_key(ctg_id) {
+                // Redis counter
+                let serial: i32 = conn.incr(format!("cnt:range:{}", ctg_id), 0).unwrap();
+                serial_of.insert(ctg_id.to_string(), serial);
+            }
+
+            let serial = serial_of.get_mut(ctg_id).unwrap();
+            *serial += 1;
             let range_id = format!("range:{}:{}", ctg_id, serial);
 
-            let _: () = redis::pipe()
+            batch = batch
                 .hset(&range_id, "chr_id", rg.chr())
                 .ignore()
                 .hset(&range_id, "chr_start", *rg.start())
                 .ignore()
                 .hset(&range_id, "chr_end", *rg.end())
-                .ignore()
-                .query(&mut conn)
-                .unwrap();
+                .ignore();
         }
 
-        // total number of ranges
-        let n_range = gars::get_scan_count(&mut conn, "range:*".to_string());
-        eprintln!("There are {} ranges in total", n_range);
+        // There could left records in stmts
+        {
+            let _: () = batch.query(&mut conn).unwrap();
+            batch.clear();
+        }
+
+        // Write increased serials back to Redis
+        for (ctg_id, serial) in &serial_of {
+            let _: () = conn.set(format!("cnt:range:{}", ctg_id), serial).unwrap();
+        }
     }
 
     Ok(())
